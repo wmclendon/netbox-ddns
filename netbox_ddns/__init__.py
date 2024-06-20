1be70ee8ba2d4e536975cc499b45b7ca1e95fabd
Will McLendon <wimclend@gmail.com>
2024-06-20 08:38:13 -0400

    fix init.py

--- netbox_ddns/__init__.py
+++ netbox_ddns/__init__.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-VERSION = '1.2.10'
-=======
 VERSION = '1.3.0'
->>>>>>> 9c8071bf33dea60827ba749fbf84f6dbf28de5b9
 
 try:
     from netbox.plugins import PluginConfig
@@ -11,18 +7,12 @@ except ImportError:
     class PluginConfig:
         pass
 
-
 class NetBoxDDNSConfig(PluginConfig):
     name = 'netbox_ddns'
     verbose_name = 'Dynamic DNS'
     version = VERSION
-<<<<<<< HEAD
-    min_version = '3.0.0'
-    max_version = '3.7.999'
-=======
     min_version = '4.0.0'
     max_version = '4.0.999'
->>>>>>> 9c8071bf33dea60827ba749fbf84f6dbf28de5b9
     author = 'Sander Steffann'
     author_email = 'sander@steffann.nl'
     description = 'Dynamic DNS Connector for NetBox'
